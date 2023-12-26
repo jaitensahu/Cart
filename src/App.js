85abbce0d131e4fe804a49030c7f47a14d0a0277
Jaiten <Jaitensahu9@gmail.com>
2023-12-26 13:51:22 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Product from './Components/Products/Product';
 import { useState } from 'react';
 
 function App() {
-  const [cartCountChange, setCartCountChange] = useState();
-  function contChange(currentCount){
-    // console.log(currentCount);
+  const [cartCountChange, setCartCountChange] = useState(4);
+  let [totalprice, setTotalAmount]=useState(2495);
+  function contChange(currentCount, Amount){
     setCartCountChange(currentCount);
+    totalprice=0;
+    console.log(Amount);
+    setTotalAmount(Amount)
   }
  
   let productArrayOfObj=[
     {
+      "id":1,
     "img":"https://www.course-api.com/images/cart/phone-1.png",
     "title": "Samsung",
     "price":399,
     "number":1
   },
   {
+    "id":2,
     "img":"https://www.course-api.com/images/cart/phone-1.png",
     "title": "xaomi",
     "price":399,
     "number":1
   },
   {
+    "id":3,
     "img":"https://www.course-api.com/images/cart/phone-1.png",
     "title": "oneplus",
     "price":699,
     "number":1
   },
   {
+    "id":4,
     "img":"https://www.course-api.com/images/cart/phone-1.png",
     "title": "Nokia",
     "price":599,
     "number":1
   },
   {
+    "id":5,
     "img":"https://www.course-api.com/images/cart/phone-1.png",
     "title": "Apple",
     "price":399,
@@ -47,10 +54,9 @@ function App() {
   const [productArray, setProductArray]=useState([...productArrayOfObj]);
   
   function dataUpdate(newData){
-    setProductArray([...productArray, newData]);
-    console.log(productArray);
+    setProductArray([...newData]);
   }
-
+// let totalprice=0;
 
   return (
     <div className="App">
@@ -59,14 +65,22 @@ function App() {
      <div className="allProductContainer">
      <h1>Your Bag</h1>
     {
-      productArrayOfObj.map(prod=>{
-        if(prod.number>0){
-          return <Product countChange={contChange} pushOrremove={dataUpdate} {...prod} wholeArr={productArrayOfObj}/>
-        }
+      productArray.map(prod=>{
+        // totalprice+=prod.price 
+        if(prod.number>0) 
+        return <Product key={prod.id} countChange={contChange} pushOrremove={dataUpdate} {...prod} wholeArr={productArray}/>
+          
       })
+      
     }
-    
+    {/* {consototalprice); */}
+     <hr />
+     <div className="totalAmount">
+        <h1>Total</h1>
+        <h2>${totalprice}</h2>
+      </div>
      </div>
+      
     </div>
   );
 }
--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -1,49 +1,75 @@
-import { useState } from "react";
 import "./Product.css";
 
 const Product = (props) => {
-  console.log();
-// Decreasing Product Count
-  function decreaseCartCount() {
-    if (productCount > 0) {
-      let updatedVal = productCount - 1;
-      setProductCount(updatedVal);
-      props.countChange(updatedVal);
-    }
-    if(productCount==0){
-        // props.pushOrremove()
-    }
-  }
-  let [num, setNum]=useState(props.number);
-//   Increasing Product Count
-  function increaseCartCount(idx) {
-    let updatedVal = productCount + 1;
-    props.pushOrremove(updatedVal)
-    setProductCount(updatedVal);
-    props.countChange(updatedVal);
+  let totalProductCount = 0;
+  let totalPrice = 0;
+  let totalAmount = 0;
+  // Decreasing Product Count
+  function decreaseCartCount(e) {
+    let proId = e.target.parentElement.parentElement.children[0].innerText;
+    let newArr = props.wholeArr.map((ele) => {
+      if (ele.id == proId) {
+        ele.number -= 1;
+      }
+      if (ele.number > 0) {
+        totalPrice = ele.price * ele.number;
+      }
+      totalAmount += totalPrice;
+      totalProductCount += ele.number;
+      return ele;
+    });
+    console.log(totalAmount);
+    props.pushOrremove(newArr);
+    props.countChange(totalProductCount, totalAmount);
   }
 
-  let [productCount, setProductCount] = useState(1);
+  //   Increasing Product Count
+  function increaseCartCount(e) {
+    let proId = e.target.parentElement.parentElement.children[0].innerText;
+    let newArr = props.wholeArr.map((ele) => {
+      if (ele.id == proId) {
+        ele.number += 1;
+      }
+      if (ele.number > 0) {
+        totalPrice = ele.price * ele.number;
+      }
+      totalAmount += totalPrice;
+      totalProductCount += ele.number;
+      return ele;
+    });
+    props.pushOrremove(newArr);
+    props.countChange(totalProductCount, totalAmount);
+  }
+  function removeData(e) {
+    let proId = e.target.parentElement.parentElement.parentElement.children[0].innerText;
+    
+let newArr = props.wholeArr.map((ele) => {
+      if (ele.id == proId) {
+        ele.number = 0;
+      }
+      return ele;
+    });
+    props.pushOrremove(newArr);
+  }
 
   return (
     <>
       <div className="ProductContainer">
+        <span id="prodID">{props.id}</span>
         <div className="productData">
-          <img
-            src={props.img}
-            alt=""
-          />
+          <img src={props.img} alt="" />
           <div className="title-price">
             <h1>{props.title}</h1>
-            <p>{props.price}</p>
-            <span>remove</span>
+            <p>$ {props.price}</p>
+
+            <span onClick={removeData}>Remove</span>
           </div>
         </div>
 
         <div className="cartValue">
-          <i class="fa-solid fa-angle-up" onClick={increaseCartCount}></i>
-          <p>{num}</p>
-          <i class="fa-solid fa-angle-down" onClick={decreaseCartCount}></i>
+          <i className="fa-solid fa-angle-up" onClick={increaseCartCount}></i>
+          <p>{props.number}</p>
+          <i className="fa-solid fa-angle-down" onClick={decreaseCartCount}></i>
         </div>
       </div>
     </>
